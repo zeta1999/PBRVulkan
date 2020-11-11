cc36262e85f142f2be9556ca8898b8bd4eb25d06
Wojciech Zielonka <wojtas.zielonka@gmail.com>
2020-11-11 14:26:58 +0100

    release 1.0 update

--- PBRVulkan/RayTracer/src/Assets/Shaders/Common/Sampling.glsl
+++ PBRVulkan/RayTracer/src/Assets/Shaders/Common/Sampling.glsl
@@ -2,37 +2,6 @@
  * Set of different sampling functions
  */
 
-vec3 cosineSampleHemisphere()
-{
-	float r1 = rnd(seed);
-	float r2 = rnd(seed);
-
-	vec3 dir;
-	float r = sqrt(r1);
-	float phi = 2.0 * PI * r2;
-
-	dir.x = r * cos(phi);
-	dir.y = r * sin(phi);
-	dir.z = sqrt(max(0.0, 1.0 - dir.x*dir.x - dir.y*dir.y));
-
-	return dir;
-}
-
-vec3 uniformSampleSphere()
-{
-	float r1 = rnd(seed);
-	float r2 = rnd(seed);
-
-	float z = 1.0 - 2.0 * r1;
-	float r = sqrt(max(0.f, 1.0 - z * z));
-	float phi = 2.0 * PI * r2;
-
-	float x = r * cos(phi);
-	float y = r * sin(phi);
-
-	return vec3(x, y, z);
-}
-
 void sampleAreaLight(in Light light, out LightSample lightSample)
 {
 	float r1 = rnd(seed);
